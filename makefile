d8b506335405fa42cf4f8fea26808acf6bdb83cb
Kevin Campuzano <kcampuzanocastillo@gmail.com>
2021-03-19 11:39:11 -0500

    close #25 asignando gestor de tareas al proyecto

---
+++ makefile
@@ -0,0 +1,8 @@
+hello: 
+	echo "hello"
+
+build: 
+	go build -o bin/main src/main.go
+
+run:
+	go run src/main.go
---
+++ src/Model/Practice/Practice.go
@@ -0,0 +1,13 @@
+package Practice
+
+type Practice struct {
+	name         string
+	initDate     string
+	endDate      string
+	timeDuration string
+}
+
+func New(name string, initDate string, endDate string, timeDuration string) Practice {
+	p := Practice{name, initDate, endDate, timeDuration}
+	return p
+}
